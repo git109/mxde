db9ee3c50c7fedaacd3eb909a2fea57be2996eae
medusade <medusade@cox.net>
2012-11-14 09:29:58 -0800

    Updated doxygen documentation

--- xos/doc/doxygen/html/search/all_61.js
+++ xos/doc/doxygen/html/search/all_61.js
@@ -2,10 +2,10 @@ var searchData=
 [
   ['acquiredinterface',['AcquiredInterface',['../classxos_1_1_acquired_interface.html',1,'xos']]],
   ['address',['Address',['../classxos_1_1network_1_1_address.html',1,'xos::network']]],
-  ['address',['Address',['../classxos_1_1network_1_1local_1_1_address.html',1,'xos::network::local']]],
-  ['address',['Address',['../classxos_1_1network_1_1ip_1_1_address.html',1,'xos::network::ip']]],
-  ['address',['Address',['../classxos_1_1network_1_1ip_1_1v6_1_1_address.html',1,'xos::network::ip::v6']]],
   ['address',['Address',['../classxos_1_1network_1_1ip_1_1v4_1_1_address.html',1,'xos::network::ip::v4']]],
+  ['address',['Address',['../classxos_1_1network_1_1ip_1_1v6_1_1_address.html',1,'xos::network::ip::v6']]],
+  ['address',['Address',['../classxos_1_1network_1_1ip_1_1_address.html',1,'xos::network::ip']]],
+  ['address',['Address',['../classxos_1_1network_1_1local_1_1_address.html',1,'xos::network::local']]],
   ['addressinterface',['AddressInterface',['../classxos_1_1network_1_1_address_interface.html',1,'xos::network']]],
   ['addressstring',['AddressString',['../classxos_1_1network_1_1ip_1_1v4_1_1_address_string.html',1,'xos::network::ip::v4']]],
   ['array',['Array',['../classxos_1_1_array.html',1,'xos']]],
--- xos/doc/doxygen/html/search/all_6c.js
+++ xos/doc/doxygen/html/search/all_6c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['level',['Level',['../classxos_1_1_logger_1_1_level.html',1,'xos::Logger']]],
   ['levels',['Levels',['../classxos_1_1_logger_1_1_levels.html',1,'xos::Logger']]],
+  ['locale',['Locale',['../classxos_1_1fs_1_1_time_1_1_locale.html',1,'xos::fs::Time']]],
   ['location',['Location',['../classxos_1_1_logger_1_1_location.html',1,'xos::Logger']]],
   ['lockedattached',['LockedAttached',['../classxos_1_1_locked_attached.html',1,'xos']]],
   ['lockedinterface',['LockedInterface',['../classxos_1_1_locked_interface.html',1,'xos']]],
